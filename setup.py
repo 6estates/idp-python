84a02344c0006af1d3069e3f4461437d48fb5ea2
6estates <103636079+linhao71@users.noreply.github.com>
2022-05-22 03:59:21 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,16 @@ from setuptools import setup
 import sixe_idp
 
 setup(
-    name='6estates_idp',
+    name='se_idp',
     version=sixe_idp.__version__,
     description='Python SDK for IDP API by 6Estates Pte Ltd',
+    long_description="""
+6Estates IDP SDK
+==================
+
+Read more on https://idp-sdk-doc.nextcenter.net/python/
+    
+    """,
     author='guosihan',
     author_email='sihan.6estates@gmail.com',
     url="https://gitlab.6estates.com/algorithm/idp-python-sdk",
